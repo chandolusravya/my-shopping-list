28a7856bee7692a28cc512e41f076fd262d4db2b
chandolusravya <chandolusravya123@gmail.com>
2024-06-25 15:41:33 +0530

    JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+const products=[
+  {name: 'cabbage', isFruit: false, id:1},
+  {name:'carrot', isFruit: true, id:2}
+]
 
-function App() {
+export default function Shopping(){
+  const listitems=products.map(product => 
+      <li key={product.id}>
+          {product.name},
+          {product.id}
+      </li>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>{listitems}</ul>
+  )
+*/
+
+//counting number of times button is clicked!
+
+
+import {useState} from "react";
+
+/*
+function MyButton(){
+
+  const [count,setCount]=useState(0);
+  //count is the current state and setcount is the function to update it
+
+  function handleClick(){
+    setCount(count+1)
+  }
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+export default function counting(){
+  return (
+    <div>
+      <MyButton/>
+      <MyButton/>
+    </div>
+  )
+}
+}*/
+
+
+
+// to update both buttons together!
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
 
-export default App;
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
